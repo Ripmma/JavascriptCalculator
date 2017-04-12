52cfbe6eb17c218d2f142f0bc9edebe78364578c
Ripmma <Ripmma@gmail.com>
2017-04-11 20:54:37 -0400

    Init Files

---
+++ index.js
@@ -0,0 +1,23 @@
+function add() {
+	// console.log("Let's add");
+	//var math = ;
+	console.log(7 + 7);
+}
+
+function subtract(){
+	//console.log("Let's subtract");
+	//var math = ;
+	console.log(7 - 7);
+}
+
+function multiply() {
+	//console.log("Let's multiply");
+	//var math = ;
+	console.log(7 * 7);
+}
+
+function divide(){
+	//console.log("Let's divide");
+	//var math = ;
+	console.log(7 / 7);
+}
